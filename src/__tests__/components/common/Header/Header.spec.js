4b5d7500b94dd5600eb4dfb65266f192f3ab3fa7
Peterson <jospeterson@deloitte.com>
2022-08-08 10:09:35 -0700

    linting errors gone

--- src/__tests__/components/common/Header/Header.spec.js
+++ src/__tests__/components/common/Header/Header.spec.js
@@ -1,31 +1,31 @@
-import { act, fireEvent, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react'
 
-import Header from '@/components/common/Header';
-import singletonRouter from 'next/router';
+import Header from '@/components/common/Header'
+import singletonRouter from 'next/router'
 
 jest.mock('@/store/store', () =>
   jest.fn(() => ({
     userData: { user: { name: 'Test user', type: 'CareerManager' } },
     setUserData: jest.fn(),
-    removeUserData: jest.fn(),
+    removeUserData: jest.fn()
   }))
 );
 
-jest.mock('next/dist/client/router', () => require('next-router-mock'));
+jest.mock('next/dist/client/router', () => require('next-router-mock'))
 
 describe('Header Component', () => {
   it('should render the component', () => {
-    const { getByText } = render(<Header />);
-    expect(getByText(/Test user/i)).toBeInTheDocument();
-  });
+    const { getByText } = render(<Header />)
+    expect(getByText(/Test user/i)).toBeInTheDocument()
+  })
 
   it('should render call the route navigation', () => {
-    singletonRouter.push('/dashboard');
-    const { getByText } = render(<Header />);
+    singletonRouter.push('/dashboard')
+    const { getByText } = render(<Header />)
     act(() => {
-      fireEvent.click(getByText('Test user'));
-    });
+      fireEvent.click(getByText('Test user'))
+    })
 
-    expect(singletonRouter).toMatchObject({ asPath: '/' });
-  });
-});
+    expect(singletonRouter).toMatchObject({ asPath: '/' })
+  })
+})
