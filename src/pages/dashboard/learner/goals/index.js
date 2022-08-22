f6b6401bfb7d17846a02e561c3f7d34daff699ea
Peterson <jospeterson@deloitte.com>
2022-08-22 10:14:32 -0700

    goals page basics created no lint or build errors

--- src/pages/dashboard/learner/goals/index.js
+++ src/pages/dashboard/learner/goals/index.js
@@ -4,8 +4,12 @@ import Table from '@/components/common/Table';
 import Tabs from '@/components/Tabs';
 import useStore from '@/store/store';
 export default function GoalsPage() {
+    const handleClick = (id) => {
+        router.push(`/dashboard/learner/competencies/${id}`);
+    };
     const userData = useStore((state) => state.userData);
-
+    const keys = ['competencyid', 'competencyframeworktitle', 'recordstatus'];
+    const cols = ['Competency ID', 'Competency Title', 'Competency Status'];
     const tabNames = ['Awarded', 'In Progress']
     const components = [<Table
         data={userData?.learner?.competencies}
