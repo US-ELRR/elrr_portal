dab167ddcf584231c81ea6cbc44f60098cafd0bc
schittlur <schittlur@deloitte.com>
2022-09-02 15:07:15 -0400

    Updating tab name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps, title = 'DOD' }) {
+function MyApp({ Component, pageProps, title = 'Enterprise Learner Record Repository' }) {
   return (
     <>
       <Head>
