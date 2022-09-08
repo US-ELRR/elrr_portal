47e83fe4c7af11139941e5511f4baf8dbc0c0165
schittlur <schittlur@deloitte.com>
2022-09-08 17:03:25 -0400

    Button component test

---
+++ src/__tests__/components/Button.test.js
@@ -0,0 +1,15 @@
+import Button from "@/components/Button";
+import { act, fireEvent, render } from "@testing-library/react";
+
+describe("Button Component", () => {
+  it("should render the component", () => {
+    const { getByText } = render(<Button btnText={"Test Button"} link={"www.google.com"} newTabLink={"/test"}/>);
+    expect(getByText(/Test Button/i)).toBeInTheDocument();
+    
+    const button = getByText('Test Button');
+    act(() => {
+        fireEvent.click(button);
+    });
+
+  });
+});
