033c3c42d0d71812fb6c9b66893444d9f26f982e
schittlur <schittlur@deloitte.com>
2022-09-08 12:32:10 -0400

    header and footer unit tests

--- src/__tests__/components/common/Footer.spec.js
+++ src/__tests__/components/common/Footer.spec.js
@@ -4,6 +4,13 @@ import Footer from "@/components/common/Footer";
 describe("Footer Component", () => {
   it("should render the component", () => {
     const { getByText } = render(<Footer />);
-    expect(getByText(/Copyright © 2022/i)).toBeInTheDocument();
+    expect(getByText(/Copyright © - 2022/i)).toBeInTheDocument();
+    expect(getByText(/Disclaimer: The data on this website is for informational purposes/i)).toBeInTheDocument();
+    expect(getByText(/DOD Home Page/i)).toBeInTheDocument();
+    expect(getByText(/About ADL/i)).toBeInTheDocument();
+    expect(getByText(/Web Policy/i)).toBeInTheDocument();
+    expect(getByText(/Privacy/i)).toBeInTheDocument();
+    expect(getByText(/Contact Us/i)).toBeInTheDocument();
+
   });
 });
--- src/__tests__/components/common/Header/Header.spec.js
+++ src/__tests__/components/common/Header/Header.spec.js
@@ -5,7 +5,16 @@ import singletonRouter from 'next/router'
 
 jest.mock('@/store/store', () =>
   jest.fn(() => ({
-    userData: { user: { name: 'Test user', type: 'CareerManager' } },
+    userData: 
+    { 
+      role:'Career Manager',
+      learner:
+        {personnel:
+          {person:
+            { name: 'Test user' } 
+          }
+        }
+    },
     setUserData: jest.fn(),
     removeUserData: jest.fn()
   }))
@@ -16,14 +25,15 @@ jest.mock('next/dist/client/router', () => require('next-router-mock'))
 describe('Header Component', () => {
   it('should render the component', () => {
     const { getByText } = render(<Header />)
-    expect(getByText(/Test user/i)).toBeInTheDocument()
+    expect(getByText(/Test user/i)).toBeInTheDocument();
+    expect(getByText("Career Manager")).toBeInTheDocument();
   })
 
   it('should render call the route navigation', () => {
     singletonRouter.push('/dashboard')
     const { getByText } = render(<Header />)
     act(() => {
-      fireEvent.click(getByText('Test user'))
+      fireEvent.click(getByText('Logout'))
     })
 
     expect(singletonRouter).toMatchObject({ asPath: '/' })
