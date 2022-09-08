26ca180c788bfb603bc823ad27fc892e787fff15
schittlur <schittlur@deloitte.com>
2022-09-08 13:16:29 -0400

    Memory router addition

--- src/__tests__/components/manager/common/CompletedCompetencies.spec.js
+++ src/__tests__/components/manager/common/CompletedCompetencies.spec.js
@@ -1,42 +1,50 @@
 import { render } from '@testing-library/react';
 import CompletedCompetencies from '@/components/manager/common/CompletedCompetencies';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 
 describe('CompletedCompetencies Component', () => {
   it('should render the component', () => {
     const { getByText } = render(
-      <CompletedCompetencies
-        completedCompetencies={[
-          {
-            competencyframeworkvalidenddate: '2020-01-01',
-            competencyframeworktitle: 'Bio',
-          },
-        ]}
-      />
+      <MemoryRouterProvider>
+        <CompletedCompetencies
+          completedCompetencies={[
+            {
+              competencyframeworkvalidenddate: '2020-01-01',
+              competencyframeworktitle: 'Bio',
+            },
+          ]}
+        />
+      </MemoryRouterProvider>
     );
     expect(getByText(/Completed Competencies/i)).toBeInTheDocument();
   });
 
   it('should render the component with no courses', () => {
     const { getByText } = render(
-      <CompletedCompetencies completedCompetencies={[]} />
+      <MemoryRouterProvider>
+        <CompletedCompetencies completedCompetencies={[]} />
+      </MemoryRouterProvider>
     );
     expect(getByText(/No competencies completed/i)).toBeInTheDocument();
   });
 
   it('should render the component with multiple courses', () => {
     const { getByText } = render(
-      <CompletedCompetencies
-        completedCompetencies={[
-          {
-            competencyframeworkvalidenddate: '2020-01-01',
-            competencyframeworktitle: 'Bio',
-          },
-          {
-            competencyframeworkvalidenddate: '2020-01-02',
-            competencyframeworktitle: 'Math',
-          },
-        ]}
-      />
+      <MemoryRouterProvider>
+        <CompletedCompetencies
+          completedCompetencies={[
+            {
+              competencyframeworkvalidenddate: '2020-01-01',
+              competencyframeworktitle: 'Bio',
+            },
+            {
+              competencyframeworkvalidenddate: '2020-01-02',
+              competencyframeworktitle: 'Math',
+            },
+          ]}
+        />
+      </MemoryRouterProvider>
+
     );
     expect(getByText(/Bio/i)).toBeInTheDocument();
     expect(getByText(/2020-01-01/i)).toBeInTheDocument();
--- src/__tests__/pages/index.test.js
+++ src/__tests__/pages/index.test.js
@@ -1,24 +1,27 @@
 // tests for the index (login) page
 import { render, screen } from '@testing-library/react';
-
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 import LoginPage from '@/pages/index';
 
+const renderer = () => {
+  return render(
+    <MemoryRouterProvider>
+        <LoginPage />
+      </MemoryRouterProvider>
+  );
+};
 describe('index page', () => {
   it('should render the login page', () => {
-    render(<LoginPage />);
-
-    expect(
-      screen.getByText('Welcome to the Enterprise Learner Record Repository')
+    const screen = renderer();
+    expect(screen.getByText('Welcome to the Enterprise Learner Record Repository')
     ).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
   });
-  it('should display the login form', () => {
-    render(<LoginPage />);
+  it('should display the login form and button', () => {
+    const screen = renderer();
+
     expect(screen.getByPlaceholderText('username')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('password')).toBeInTheDocument();
-  });
-  it('should display the login button', () => {
-    render(<LoginPage />);
     expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
   });
 });
