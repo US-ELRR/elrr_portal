6515ed99f5bddcb19dfbccc1d4622429392c32b3
schittlur <schittlur@deloitte.com>
2022-09-09 13:33:15 -0400

    Search component test

---
+++ src/__tests__/components/Search.test.js
@@ -0,0 +1,23 @@
+import Search from "@/components/Search";
+import { act, fireEvent, getByPlaceholderText, render } from "@testing-library/react";
+
+describe("Search Component", () => {
+  it("should render the component", () => {
+    const testValue = "testValue"
+    const { getByText, getByPlaceholderText } = render(
+    <Search searchQuery={"Test Search"} 
+        setSearchQuery={(testValue) => {
+            console.log(testValue); 
+        }}/>);
+
+        expect(getByPlaceholderText('Search...')).toBeInTheDocument();
+    expect(getByPlaceholderText('Search...').value).toBe('Test Search');
+    
+    act(() => {
+        fireEvent.input(getByPlaceholderText('Search...'), {
+          target: { value: 'testValue' },
+        });
+    });
+
+  });
+});
