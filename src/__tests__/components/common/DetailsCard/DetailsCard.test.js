642dbdbbf4ecff68f2ddbb9fcb72a51f6d4d9fde
schittlur <schittlur@deloitte.com>
2022-09-09 14:41:55 -0400

    DetailsCard test

---
+++ src/__tests__/components/common/DetailsCard/DetailsCard.test.js
@@ -0,0 +1,19 @@
+import DetailsCard from "@/components/common/DetailsCard";
+import { render } from "@testing-library/react";
+
+describe("DetailsCard Component", () => {
+    
+  it("should render the component", () => {
+    const { getByText } = render(<DetailsCard obj={["comp1", "comp2", "comp3"]} title='Competency' cols={3}/>);
+    expect(getByText(/Competency/i)).toBeInTheDocument();
+    expect(getByText(/comp1/i)).toBeInTheDocument();
+    expect(getByText(/comp2/i)).toBeInTheDocument();
+    expect(getByText(/comp3/i)).toBeInTheDocument();
+  });
+
+  it("should render the component if no data", () => {
+    const { getByText } = render(<DetailsCard title='Competency' cols={3}/>);
+    expect(getByText(/Competency/i)).toBeInTheDocument();
+    expect(getByText(/No data to display/i)).toBeInTheDocument();
+  });
+});
