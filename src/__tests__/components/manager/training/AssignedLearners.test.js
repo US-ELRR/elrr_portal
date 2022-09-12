5b4df2f2ecf607833d88d462859d7bedd9a872ad
schittlur <schittlur@deloitte.com>
2022-09-12 13:36:02 -0400

    assignedLearners test

---
+++ src/__tests__/components/manager/training/AssignedLearners.test.js
@@ -0,0 +1,34 @@
+import AssignedLearners from "@/components/manager/training/AssignedLearners";
+import { act, fireEvent, render } from "@testing-library/react";
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+
+describe("AssignedLearners Component", () => {
+    
+    it("should render the component", () => {
+        const learners=[
+            {
+                personnel: {
+                    person: {
+                        personid: "1234",
+                        name: "John Doe"
+                    }
+                }
+            },
+        ]
+        const { getByText } = render(
+            <MemoryRouterProvider>
+                <AssignedLearners learners={learners}/>
+            </MemoryRouterProvider> );
+            
+        expect(getByText("Assigned Learners")).toBeInTheDocument();
+        expect(getByText(/Id/i)).toBeInTheDocument();
+        expect(getByText(/Name/i)).toBeInTheDocument();
+        expect(getByText(/1234/i)).toBeInTheDocument();
+        expect(getByText(/John Doe/i)).toBeInTheDocument();
+        
+        const button = getByText('1234');
+        act(() => {
+            fireEvent.click(button);
+        });
+    });
+});
