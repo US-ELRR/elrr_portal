db3b4f37c9ee6372ab4df87755488801dcc88234
schittlur <schittlur@deloitte.com>
2022-09-12 11:51:48 -0400

    LearnerDashboard tests

---
+++ src/__tests__/components/learner/LearnerDashboard.test.js
@@ -0,0 +1,32 @@
+import LearnerDashboard from "@/components/learner/LearnerDashboard";
+import { act, fireEvent, render } from "@testing-library/react";
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+
+describe("LearnerDashboard Component", () => {
+    
+    it("should render the component", () => {
+        const data = [
+            {
+                id: "123",
+                title: "test title",
+                description: ""
+            }
+        ]
+        const { getByText } = render(
+            <MemoryRouterProvider>
+            <LearnerDashboard />
+            </MemoryRouterProvider> );
+        expect(getByText(/Welcome/i)).toBeInTheDocument();
+        expect(getByText(/to the Enterprise Learner Record Repository/i)).toBeInTheDocument();
+        expect(getByText(/(ELRR)/i)).toBeInTheDocument();
+        expect(getByText(/Enrolled Courses/i)).toBeInTheDocument();
+        expect(getByText(/Course Identifier/i)).toBeInTheDocument();
+        expect(getByText(/Course Name/i)).toBeInTheDocument();
+        expect(getByText(/Start Date/i)).toBeInTheDocument();
+        expect(getByText(/Completed Competencies/i)).toBeInTheDocument();
+        expect(getByText(/Title/i)).toBeInTheDocument();
+        expect(getByText(/Conferred Date/i)).toBeInTheDocument();
+        expect(getByText(/No competencies completed/i)).toBeInTheDocument();
+
+    });
+});
