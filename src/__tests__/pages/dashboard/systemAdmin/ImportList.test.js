0e5e79b4aeef6753d0738999f9f43fc2724cbf9c
schittlur <schittlur@deloitte.com>
2022-09-13 18:49:04 -0400

    Import List page test

---
+++ src/__tests__/pages/dashboard/systemAdmin/ImportList.test.js
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+import ImportList from "@/pages/dashboard/systemAdmin/ImportList";
+
+describe("ImportList Component", () => {
+    it("should render the component", () => {
+        const { getByText } = render(
+            <MemoryRouterProvider>
+                <ImportList />
+            </MemoryRouterProvider> );
+            
+        expect(getByText("Import List")).toBeInTheDocument();
+        expect(getByText("Date")).toBeInTheDocument();
+        expect(getByText("Total Imports")).toBeInTheDocument();
+        expect(getByText("Total Failures")).toBeInTheDocument();
+        expect(getByText("Endpoint")).toBeInTheDocument();
+    });
+});
