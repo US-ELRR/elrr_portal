db1cbeb45fb1766f952e25803c2f7e965e2f6a6a
schittlur <schittlur@deloitte.com>
2022-09-14 12:54:43 -0400

    userProfileManagement test

---
+++ src/__tests__/pages/dashboard/systemAdmin/UserProfileManagement.test.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+import UserProfileManagement from "@/pages/dashboard/systemAdmin/UserProfileManagement";
+
+describe("UserProfileManagement Component", () => {
+    it("should render the component", () => {
+        const { getByText, getAllByText } = render(
+            <MemoryRouterProvider>
+                <UserProfileManagement />
+            </MemoryRouterProvider> );
+            
+        expect(getByText("Brinleigh Blanchard")).toBeInTheDocument();
+        expect(getAllByText("Learner").length).toBe(5);
+        expect(getAllByText("Active").length).toBe(8);
+    });
+});
