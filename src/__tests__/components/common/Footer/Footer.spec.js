91816b9461eaf503e3789cbddd9b3b4f28a9b6b6
schittlur <schittlur@deloitte.com>
2022-09-22 12:42:00 -0400

    unit test updates based on P1

--- src/__tests__/components/common/Footer/Footer.spec.js
+++ src/__tests__/components/common/Footer/Footer.spec.js
@@ -4,7 +4,7 @@ import Footer from "@/components/common/Footer";
 describe("Footer Component", () => {
   it("should render the component", () => {
     const { getByText } = render(<Footer />);
-    expect(getByText(/Copyright © - 2022/i)).toBeInTheDocument();
+    expect(getByText("Copyright © - 2022")).toBeInTheDocument();
     expect(getByText(/Disclaimer: The data on this website is for informational purposes/i)).toBeInTheDocument();
     expect(getByText(/DOD Home Page/i)).toBeInTheDocument();
     expect(getByText(/About ADL/i)).toBeInTheDocument();
--- src/__tests__/components/common/Header/LoggedOutNavBar.spec.js
+++ src/__tests__/components/common/Header/LoggedOutNavBar.spec.js
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
 import LoggedOutNavbar from '@/components/common/Header/LoggedOutNavBar';
+import { render } from '@testing-library/react';
 
 describe('LoggedOutNavbar Component', () => {
   it('should render the component with the correct text', () => {
