538732198091bd93e3cd2d8ddae28b83d7dd381d
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2022-12-14 15:39:56 -0500

    Update PlotAndChart.test.js
    Made changes to test from line 88 to 104 to improve code coverage.
    Added test for error handling in line 106
    Also, formatted the test file for better readability.

--- src/__tests__/components/manager/career/Plots/Charts/PlotAndChart.test.js
+++ src/__tests__/components/manager/career/Plots/Charts/PlotAndChart.test.js
@@ -3,94 +3,111 @@ import { act, render } from "@testing-library/react";
 import CompetenciesRadarChart from "@/components/manager/career/CompetenciesRadarChart";
 import EmploymentCourseScatterPlot from "@/components/manager/career/EmploymentCourseScatterPlot";
 import axios from "axios";
-import courseData from '@/data/courseData.json';
 
 jest.mock('axios');
 
 describe("EmploymentCourseScatterPlot Component", () => {
-    const employmentData = {
-        "history": [
-            {
-              "employment": "Army",
-              "courses": [
-                {
-                  "status": "ABANDONED"
-                },
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "ABANDONED"
-                },
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "COMPLETED"
-                }
-              ]
-            },
-            {
-              "employment": "Navy",
-              "courses": [
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "COMPLETED"
-                }
-              ]
-            },
-            {
-              "employment": "Marines",
-              "courses": [
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "ABANDONED"
-                }
-              ]
-            },
-            {
-              "employment": "Air Force",
-              "courses": [
-                {
-                  "status": "REGISTERED"
-                },
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "COMPLETED"
-                },
-                {
-                  "status": "COMPLETED"
-                }
-              ]
-            }
-          ]
-    }
-    it("should render the component", () => {
-        act(() => {
-          axios.get.mockImplementation(() => Promise.resolve({ data: {employmentData} }));
-        });
-          render(
-            <MemoryRouterProvider>
-                <EmploymentCourseScatterPlot userId={"101"} />
-            </MemoryRouterProvider> );
+  const employmentData = {
+    "history": [
+      {
+        "employment": "Army",
+        "courses": [
+          {
+            "status": "ABANDONED"
+          },
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "ABANDONED"
+          },
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "COMPLETED"
+          }
+        ]
+      },
+      {
+        "employment": "Navy",
+        "courses": [
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "COMPLETED"
+          }
+        ]
+      },
+      {
+        "employment": "Marines",
+        "courses": [
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "ABANDONED"
+          }
+        ]
+      },
+      {
+        "employment": "Air Force",
+        "courses": [
+          {
+            "status": "REGISTERED"
+          },
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "COMPLETED"
+          },
+          {
+            "status": "COMPLETED"
+          }
+        ]
+      }
+    ]
+  }
+  it("should render the component", () => {
+    act(() => {
+      axios.get.mockImplementation(() => Promise.resolve({ data: { employmentData } }));
     });
+    render(
+      <MemoryRouterProvider>
+        <EmploymentCourseScatterPlot userId={"101"} />
+      </MemoryRouterProvider>);
+  });
 });
 
 describe("CompetenciesRadarChart Component", () => {
-    it("should render the component", () => {
-        axios.get.mockImplementation(() => Promise.resolve({ data: {courseData} }));
-        render(
-            <MemoryRouterProvider>
-                <CompetenciesRadarChart userId={"101"} />
-            </MemoryRouterProvider> );
+  it("should render the component", async () => {
+    axios.get.mockImplementation(() => Promise.resolve({
+      data:
+      {
+        buckets: [
+          { name: "bucket_name" },
+          { name: "bucket2" }
+        ]
+      }
+    }));
+    await act(async () => {
+      render(
+        <MemoryRouterProvider>
+          <CompetenciesRadarChart userId={"101"} />
+        </MemoryRouterProvider>);
     });
+  });
+
+  it("should return an axios error", () => {
+    axios.get.mockImplementation(() => Promise.reject(new Error("Throwing an error!")));
+    render(
+      <MemoryRouterProvider>
+        <CompetenciesRadarChart userId={"101"} />
+      </MemoryRouterProvider>);
+  });
 });
