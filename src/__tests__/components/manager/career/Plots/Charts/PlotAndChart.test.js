32048f306c0b0ee453f88d3d447086655da10e22
schittlur <schittlur@deloitte.com>
2022-12-14 15:52:28 -0500

    error handling

--- src/__tests__/components/manager/career/Plots/Charts/PlotAndChart.test.js
+++ src/__tests__/components/manager/career/Plots/Charts/PlotAndChart.test.js
@@ -1,113 +1,119 @@
 import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
-import { act, render } from "@testing-library/react";
-import CompetenciesRadarChart from "@/components/manager/career/CompetenciesRadarChart";
-import EmploymentCourseScatterPlot from "@/components/manager/career/EmploymentCourseScatterPlot";
-import axios from "axios";
+import { act, render } from '@testing-library/react';
+import CompetenciesRadarChart from '@/components/manager/career/CompetenciesRadarChart';
+import EmploymentCourseScatterPlot from '@/components/manager/career/EmploymentCourseScatterPlot';
+import axios from 'axios';
 
 jest.mock('axios');
 
-describe("EmploymentCourseScatterPlot Component", () => {
+describe('EmploymentCourseScatterPlot Component', () => {
   const employmentData = {
-    "history": [
+    history: [
       {
-        "employment": "Army",
-        "courses": [
+        employment: 'Army',
+        courses: [
           {
-            "status": "ABANDONED"
+            status: 'ABANDONED',
           },
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "ABANDONED"
+            status: 'ABANDONED',
           },
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "COMPLETED"
-          }
-        ]
+            status: 'COMPLETED',
+          },
+        ],
       },
       {
-        "employment": "Navy",
-        "courses": [
+        employment: 'Navy',
+        courses: [
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "COMPLETED"
-          }
-        ]
+            status: 'COMPLETED',
+          },
+        ],
       },
       {
-        "employment": "Marines",
-        "courses": [
+        employment: 'Marines',
+        courses: [
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "ABANDONED"
-          }
-        ]
+            status: 'ABANDONED',
+          },
+        ],
       },
       {
-        "employment": "Air Force",
-        "courses": [
+        employment: 'Air Force',
+        courses: [
           {
-            "status": "REGISTERED"
+            status: 'REGISTERED',
           },
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "COMPLETED"
+            status: 'COMPLETED',
           },
           {
-            "status": "COMPLETED"
-          }
-        ]
-      }
-    ]
-  }
-  it("should render the component", () => {
+            status: 'COMPLETED',
+          },
+        ],
+      },
+    ],
+  };
+  it('should render the component', () => {
     act(() => {
-      axios.get.mockImplementation(() => Promise.resolve({ data: { employmentData } }));
+      axios.get.mockImplementation(() =>
+        Promise.resolve({ data: { employmentData } })
+      );
     });
     render(
       <MemoryRouterProvider>
-        <EmploymentCourseScatterPlot userId={"101"} />
-      </MemoryRouterProvider>);
+        <EmploymentCourseScatterPlot userId={'101'} />
+      </MemoryRouterProvider>
+    );
   });
 });
 
-describe("CompetenciesRadarChart Component", () => {
-  it("should render the component", async () => {
-    axios.get.mockImplementation(() => Promise.resolve({
-      data:
-      {
-        buckets: [
-          { name: "bucket_name" },
-          { name: "bucket2" }
-        ]
-      }
-    }));
+describe('CompetenciesRadarChart Component', () => {
+  it('should render the component', async () => {
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          buckets: [{ name: 'bucket_name' }, { name: 'bucket2' }],
+        },
+      })
+    );
+
     await act(async () => {
       render(
         <MemoryRouterProvider>
-          <CompetenciesRadarChart userId={"101"} />
-        </MemoryRouterProvider>);
+          <CompetenciesRadarChart userId={'101'} />
+        </MemoryRouterProvider>
+      );
     });
   });
 
-  it("should return an axios error", () => {
+  it("should return an axios error", async() => {
     axios.get.mockImplementation(() => Promise.reject(new Error("Throwing an error!")));
-    render(
-      <MemoryRouterProvider>
-        <CompetenciesRadarChart userId={"101"} />
-      </MemoryRouterProvider>);
+    await act(async () => {
+      render(
+        <MemoryRouterProvider>
+          <CompetenciesRadarChart userId={'101'} />
+        </MemoryRouterProvider>
+      );
+    });
   });
 });
