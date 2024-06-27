1ab53cdd8dfea94b664f5c78b3bb030f965aa4f5
schittlur <78377530+schittlur@users.noreply.github.com>
2024-06-27 11:34:42 -0400

    name update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,9 @@ export default function Banner({ user }) {
     return (
         <div className='bg-gray-300 w-full py-2 text-center'>
             <h1 className='text-3xl font-semibold'>
-                Welcome {user?.learner.personnel.person.firstName} to the Enterprise
+                {/* Welcome {user?.learner.personnel.person.firstName} to the Enterprise */}
+
+                Welcome Kara to the Enterprise
                 Learner Record Repository&nbsp;
                 <strong>(ELRR)</strong>
             </h1>
